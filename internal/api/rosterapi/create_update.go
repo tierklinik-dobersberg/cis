d87bd058ca2bd0080ad5e437fe109cd644b4069b
Patrick Pacher <patrick.pacher@gmail.com>
2021-01-10 21:18:49 +0100

    Add validation to create-roster endpoint

--- internal/api/rosterapi/create_update.go
+++ internal/api/rosterapi/create_update.go
@@ -1,14 +1,18 @@
 package rosterapi
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/tierklinik-dobersberg/cis/internal/app"
 	"github.com/tierklinik-dobersberg/cis/internal/database/rosterdb"
+	"github.com/tierklinik-dobersberg/cis/internal/schema"
 	"github.com/tierklinik-dobersberg/cis/pkg/models/roster/v1alpha"
 	"github.com/tierklinik-dobersberg/service/server"
 )
@@ -40,7 +44,16 @@ func CreateOrUpdateEndpoint(grp gin.IRouter) {
 		}
 
 		if body.Month != month || body.Year != year {
-			server.AbortRequest(c, http.StatusBadRequest, fmt.Errorf("invalid month or year in body"))
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": "invalid year or month (url match)",
+			})
+			return
+		}
+
+		if err := validateRoster(c.Request.Context(), app, &body); err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
 			return
 		}
 
@@ -57,4 +70,47 @@ func CreateOrUpdateEndpoint(grp gin.IRouter) {
 
 		c.Status(http.StatusNoContent)
 	})
+}
+
+func validateRoster(ctx context.Context, app *app.App, roster *v1alpha.DutyRoster) error {
+	// Come one, I guess 2100 is enough ...
+	if roster.Year < 2019 || roster.Year > 2100 {
+		return fmt.Errorf("invalid year")
+	}
+
+	if roster.Month < time.January || roster.Month > time.December {
+		return fmt.Errorf("invalid month")
+	}
+
+	allUsers, err := app.Identities.ListAllUsers(ctx)
+	if err != nil {
+		return err
+	}
+	lm := make(map[string]schema.User, len(allUsers))
+	for _, user := range allUsers {
+		lm[strings.ToLower(user.Name)] = user
+	}
+
+	for _, day := range roster.Days {
+		if err := validateUsers(day.Forenoon, lm); err != nil {
+			return fmt.Errorf("forenoon: %s", err)
+		}
+		if err := validateUsers(day.Afternoon, lm); err != nil {
+			return fmt.Errorf("afternoon: %s", err)
+		}
+		if err := validateUsers(day.Emergency, lm); err != nil {
+			return fmt.Errorf("emergency: %s", err)
+		}
+	}
+
+	return nil
+}
+
+func validateUsers(users []string, lm map[string]schema.User) error {
+	for _, u := range users {
+		if _, ok := lm[strings.ToLower(u)]; !ok {
+			return fmt.Errorf("unknown user %s", u)
+		}
+	}
+	return nil
 }
