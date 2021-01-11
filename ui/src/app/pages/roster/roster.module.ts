1f4a3c30bbe4dd54d5778b4802d579fff056524c
Patrick Pacher <patrick.pacher@gmail.com>
2021-01-11 14:21:24 +0100

    :tv: add simple calendar to roster

--- ui/src/app/pages/roster/roster.module.ts
+++ ui/src/app/pages/roster/roster.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from "@angular/core";
+import { NzCalendarModule } from "ng-zorro-antd/calendar";
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
 import { RosterComponent } from "./roster";
 import { RosterRoutingModule } from "./roster-routing.module";
 
 @NgModule({
   imports: [
+    NzCalendarModule,
+    NzBadgeModule,
     RosterRoutingModule,
   ],
   declarations: [
