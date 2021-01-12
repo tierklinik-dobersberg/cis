3ff94d6651630e481c77fb1fb469bba5cede5cf0
Patrick Pacher <patrick.pacher@gmail.com>
2021-01-12 18:06:18 +0100

    Add missing bson struct tags

--- pkg/models/calllog/v1alpha/model.go
+++ pkg/models/calllog/v1alpha/model.go
@@ -8,14 +8,14 @@ import (
 
 // CallLog represents an incoming call.
 type CallLog struct {
-	ID primitive.ObjectID `json:"_id,omitempty"`
+	ID primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	// Caller is the calling phone number.
-	Caller string `json:"caller"`
+	Caller string `json:"caller" bson:"caller,omitempty"`
 	// InboundNumber is the called number.
-	InboundNumber string `json:"inboundNumber"`
+	InboundNumber string `json:"inboundNumber" bson:"inboundNumber,omitempty"`
 	// Date holds the exact date the call was recorded.
-	Date time.Time `json:"date"`
+	Date time.Time `json:"date" bson:"date,omitempty"`
 	// DateStr holds a string representation of the date in
 	// the format of YYYY-MM-DD for indexing.
-	DateStr string `json:"datestr"`
+	DateStr string `json:"datestr" bson:"datestr,omitempty"`
 }
--- pkg/models/customer/v1alpha/models.go
+++ pkg/models/customer/v1alpha/models.go
@@ -3,14 +3,14 @@ package v1alpha
 // Customer represents a customer model
 type Customer struct {
 	// TODO(ppacher): should we actually expose ID?
-	ID         string `json:"_id,omitempty"`
-	CustomerID int    `json:"cid,omitempty"`
-	Group      string `json:"group,omitempty"`
-	Name       string `json:"name,omitempty"`
-	Firstname  string `json:"firstname,omitempty"`
-	Title      string `json:"title,omitempty"`
-	Street     string `json:"street,omitempty"`
-	CityCode   int    `json:"cityCode,omitempty"`
-	City       string `json:"city,omitempty"`
-	Phone      string `json:"phone,omitempty"`
+	ID         string `json:"_id,omitempty" bson:"_id,omitempty"`
+	CustomerID int    `json:"cid,omitempty" bson:"cid,omitempty"`
+	Group      string `json:"group,omitempty" bson:"group,omitempty"`
+	Name       string `json:"name,omitempty" bson:"name,omitempty"`
+	Firstname  string `json:"firstname,omitempty" bson:"firstname,omitempty"`
+	Title      string `json:"title,omitempty" bson:"title,omitempty"`
+	Street     string `json:"street,omitempty" bson:"street,omitempty"`
+	CityCode   int    `json:"cityCode,omitempty" bson:"cityCode,omitempty"`
+	City       string `json:"city,omitempty" bson:"city,omitempty"`
+	Phone      string `json:"phone,omitempty" bson:"phone,omitempty"`
 }
