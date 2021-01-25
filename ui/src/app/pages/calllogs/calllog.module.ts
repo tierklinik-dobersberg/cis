4456fc909fd0a10a2111467e01bc7d76cc8d4e49
Patrick Pacher <patrick.pacher@gmail.com>
2021-01-25 23:19:43 +0100

    Update calllogs page

--- ui/src/app/pages/calllogs/calllog.module.ts
+++ ui/src/app/pages/calllogs/calllog.module.ts
@@ -1,23 +1,29 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { NzButtonModule } from "ng-zorro-antd/button";
 import { NzCardModule } from "ng-zorro-antd/card";
+import { NzDatePickerModule } from "ng-zorro-antd/date-picker";
 import { NzGridModule } from "ng-zorro-antd/grid";
 import { NzIconModule } from "ng-zorro-antd/icon";
 import { NzStatisticModule } from "ng-zorro-antd/statistic";
 import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzToolTipModule } from "ng-zorro-antd/tooltip";
 import { CallLogComponent } from "./calllog";
 import { CallLogRoutingModule } from "./calllog-routing.module";
 
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         NzGridModule,
         NzCardModule,
         NzButtonModule,
         NzStatisticModule,
         NzIconModule,
         NzTableModule,
+        NzToolTipModule,
+        NzDatePickerModule,
         CallLogRoutingModule,
     ],
     declarations: [
--- ui/src/app/pages/calllogs/calllog.ts
+++ ui/src/app/pages/calllogs/calllog.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit, TrackByFunction } from "@angular/core";
-import { forkJoin, interval, of, Subscription } from "rxjs";
+import { BehaviorSubject, forkJoin, interval, of, Subscription } from "rxjs";
 import { catchError, mergeMap, startWith } from "rxjs/operators";
 import { CallLog, CalllogAPI } from "src/app/api";
 import { Customer, CustomerAPI } from "src/app/api/customer.api";
@@ -15,6 +15,9 @@ interface LocalCallLog extends CallLog {
 })
 export class CallLogComponent implements OnInit, OnDestroy {
     private subscriptions = Subscription.EMPTY;
+    private _selectedDate = new BehaviorSubject<Date>(new Date());
+
+    date: Date;
 
     logs: LocalCallLog[] = [];
 
@@ -31,15 +34,20 @@ export class CallLogComponent implements OnInit, OnDestroy {
         private customerapi: CustomerAPI,
     ) { }
 
+    onChange(date: Date) {
+        this.date = date;
+        this._selectedDate.next(date);
+    }
+
     ngOnInit() {
+        this.date = new Date();
         this.subscriptions = new Subscription();
 
         const sub =
-            interval(5000)
+            interval(30000)
                 .pipe(
-                    startWith(0),
-                    mergeMap(() => {
-                        const d = new Date();
+                    mergeMap(() => this._selectedDate),
+                    mergeMap(d => {
                         const year = d.getFullYear();
                         const month = d.getMonth() + 1
                         const day = d.getDate();
@@ -47,6 +55,7 @@ export class CallLogComponent implements OnInit, OnDestroy {
                         return this.calllogapi.forDate(year, month, day);
                     }),
                     mergeMap(logs => {
+                        logs = logs || [];
                         return forkJoin({
                             logs: of(logs),
                             numbers: this.customerapi.search({
