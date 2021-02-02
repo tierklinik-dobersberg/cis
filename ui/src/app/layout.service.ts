f7f3a20f346a4f1fad6f529f4825d6e50b2ba726
Patrick Pacher <patrick.pacher@gmail.com>
2021-02-02 09:12:52 +0100

    :iphone: add more breakpoints to layout service'

--- ui/src/app/layout.service.ts
+++ ui/src/app/layout.service.ts
@@ -3,22 +3,38 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
+
+
 @Injectable({
     providedIn: 'root'
 })
 export class LayoutService {
-    phone$: Observable<boolean>;
+    breakpoints: Observable<{ [key: string]: boolean }>;
 
-    isPhone: boolean
+    isPhone = false;
+    isTabletPortraitUp = false;
+    isTabletLandscapeUp = false;
+    isDesktopUp = false;
+    isBigDesktopUp = false;
 
     constructor(private breakpointObserver: BreakpointObserver) {
-        this.phone$ = this.breakpointObserver.observe('(max-width: 599px)')
+        this.breakpoints = this.breakpointObserver.observe([
+            '(max-width: 599px)',
+            '(min-width: 600px)',
+            '(min-width: 900px)',
+            '(min-width: 1200px)',
+            '(min-width: 1800px)',
+        ])
             .pipe(map(breakpointState => {
-                return breakpointState.matches;
+                return breakpointState.breakpoints;
             }));
 
-        this.phone$.subscribe(isPhone => {
-            this.isPhone = isPhone;
+        this.breakpoints.subscribe(states => {
+            this.isPhone = states['(max-width: 599px)'];
+            this.isTabletPortraitUp = states['(min-width: 600px)'];
+            this.isTabletLandscapeUp = states['(min-width: 900px)']
+            this.isDesktopUp = states['(min-width: 1200px)']
+            this.isBigDesktopUp = states['(min-width: 1800px)']
         });
     }
 }
