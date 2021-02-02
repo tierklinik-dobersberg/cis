152d5c5fc59412994864918c0aa756339053f2c7
Patrick Pacher <patrick.pacher@gmail.com>
2021-02-02 14:59:30 +0100

    :loud_sound: debug(calllogs): dump incoming requests for debugging

--- internal/api/externalapi/record_call.go
+++ internal/api/externalapi/record_call.go
@@ -2,9 +2,12 @@ package externalapi
 
 import (
 	"context"
+	"io/ioutil"
 	"net/http"
+	"net/http/httputil"
 	"time"
 
+	"github.com/apex/log"
 	"github.com/gin-gonic/gin"
 	"github.com/tierklinik-dobersberg/cis/internal/app"
 	"github.com/tierklinik-dobersberg/cis/internal/httperr"
@@ -15,8 +18,17 @@ func RecordCallEndpoint(grp *app.Router) {
 	grp.POST(
 		"v1/calllog",
 		func(ctx context.Context, app *app.App, c *gin.Context) error {
+
 			caller := c.Query("ani")
 			did := c.Query("did")
+
+			reqBlob, err := httputil.DumpRequest(c.Request, true)
+			if err != nil {
+				log.Errorf("failed to dump request: %s", err)
+			}
+			if err := ioutil.WriteFile("/log/record-call-request.dump", reqBlob, 0700); err != nil {
+				log.Errorf("failed to dump request: %s", err)
+			}
 
 			if err := app.CallLogs.Create(ctx, time.Now(), caller, did); err != nil {
 				return httperr.InternalError(err)
