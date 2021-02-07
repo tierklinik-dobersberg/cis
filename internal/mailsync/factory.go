b53bd690be81ef250a84f32e4ee00714bc13655e
Patrick Pacher <patrick.pacher@gmail.com>
2021-02-07 15:18:46 +0100

    Add basic mail syncer

--- internal/mailsync/factory.go
+++ internal/mailsync/factory.go
@@ -2,7 +2,13 @@ package mailsync
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"time"
 
+	"github.com/tierklinik-dobersberg/logger"
+	"github.com/tierklinik-dobersberg/mailbox"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -28,4 +34,37 @@ func NewManagerWithClient(ctx context.Context, dbName string, cli *mongo.Client)
 
 func (mng *Manager) setup(ctx context.Context) error {
 	return nil
+}
+
+// NewSyncer returns a new
+func (mng *Manager) NewSyncer(ctx context.Context, name string, interval time.Duration, cfg *mailbox.Config) (*Syncer, error) {
+	result := mng.syncState.FindOne(ctx, bson.M{"name": name})
+	if result.Err() != nil && !errors.Is(result.Err(), mongo.ErrNoDocuments) {
+		return nil, fmt.Errorf("loading state: %w", result.Err())
+	}
+
+	var state State
+	if result.Err() == nil {
+		if err := result.Decode(&state); err != nil {
+			return nil, fmt.Errorf("decoding state: %w", err)
+		}
+	}
+
+	cli, err := mailbox.Connect(*cfg)
+	if err != nil {
+		return nil, err
+	}
+	defer cli.IMAP.Logout(time.Second * 5)
+
+	syncer := &Syncer{
+		state:        state,
+		syncState:    mng.syncState,
+		cfg:          cfg,
+		pollInterval: interval,
+		log: logger.From(ctx).WithFields(logger.Fields{
+			"mailbox": name,
+		}),
+	}
+
+	return syncer, nil
 }
