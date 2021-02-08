49c0f2faaf4f863e0b80659f0cbfe09988a262d8
Patrick Pacher <patrick.pacher@gmail.com>
2021-02-08 13:48:21 +0100

    Fix customer not loaded

--- ui/src/app/pages/calllogs/calllog.ts
+++ ui/src/app/pages/calllogs/calllog.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit, TrackByFunction } from "@angular/core";
 import { sum } from "ng-zorro-antd/core/util";
-import { BehaviorSubject, combineLatest, forkJoin, interval, of, Subscription } from "rxjs";
+import { BehaviorSubject, combineLatest, forkJoin, interval, Observable, of, Subscription } from "rxjs";
 import { catchError, mergeMap, startWith } from "rxjs/operators";
 import { CallLog, CalllogAPI } from "src/app/api";
 import { Customer, CustomerAPI } from "src/app/api/customer.api";
@@ -63,12 +63,22 @@ export class CallLogComponent implements OnInit, OnDestroy {
           }),
           mergeMap(logs => {
             logs = logs || [];
-            let queryForNumbers = Array.from(
-              logs.reduce((res: Set<string>, l: CallLog) => {
-                res.add(l.caller);
-                return res;
-              }, new Set<string>())
-            );
+
+            let customerSet = new Map<string, [string, string]>();
+            let distinctNumbers = new Set<string>();
+
+            // get a list of distinct phone number and customer source/ID pairs.
+            logs.forEach(l => {
+              if (!!l.customerID && !!l.customerSource) {
+                customerSet.set(`${l.customerSource}/${l.customerID}`, [
+                  l.customerSource,
+                  l.customerID
+                ]);
+              }
+
+              distinctNumbers.add(l.caller.replace(" ", ""));
+            })
+            let queryForNumbers = Array.from(distinctNumbers.values());
 
             // make sure we don't send a query if there aren't any
             // numbers we want to lookup as it would return all
@@ -83,14 +93,26 @@ export class CallLogComponent implements OnInit, OnDestroy {
               }));
             }
 
+            let allCustomers: Observable<Customer[]> = of([]);
+            if (customerSet.size > 0) {
+              allCustomers = combineLatest(
+                Array.from(customerSet.values()).map(([source, id]) => {
+                  return this.customerapi.byId(source, id)
+                    .pipe(catchError(err => of(null)));
+                })
+              )
+            }
+
             return forkJoin({
               logs: of(logs),
               numbers: numbersStream,
+              customers: allCustomers,
             })
           })
         )
         .subscribe(logs => {
           let lm = new Map<string, Customer>();
+
           logs.numbers?.forEach(cust => cust.phoneNumbers?.forEach(number => {
             number = number.replace(" ", "")
             if (number === "") {
@@ -105,7 +127,15 @@ export class CallLogComponent implements OnInit, OnDestroy {
           )
 
           this.logs = logs.logs.map(l => {
-            const cust = lm.get(l.caller);
+            let cust: Customer | undefined;
+            if (!!l.customerSource) {
+              cust = logs.customers.find(c => c.source === l.customerSource && c.cid == +l.customerID);
+            }
+
+            if (!cust) {
+              cust = lm.get(l.caller);
+            }
+
             return {
               ...l,
               localDate: new Date(l.date).toLocaleString(),
