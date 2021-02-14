db62fb9069a0f725a2206a0d4c43e5703180a02a
Patrick Pacher <patrick.pacher@gmail.com>
2021-02-14 09:45:14 +0100

    :tada: feat(session): add httpcond types for SessionUser and SessionRole

---
+++ internal/session/httpconds.go
@@ -0,0 +1,49 @@
+package session
+
+import (
+	"net/http"
+
+	"github.com/ppacher/system-conf/conf"
+	"github.com/tierklinik-dobersberg/cis/internal/httpcond"
+)
+
+func init() {
+	httpcond.MustRegister(httpcond.Type{
+		Name:        "SessionUser",
+		Description: "Match against the session user.",
+		Type:        conf.StringSliceType,
+		ConcatFunc:  httpcond.NewOr,
+		Match: func(req *http.Request, value string) (bool, error) {
+			sess := FromCtx(req.Context())
+			if sess == nil {
+				return false, nil
+			}
+
+			if sess.User.Name == value {
+				return true, nil
+			}
+			return false, nil
+		},
+	})
+
+	httpcond.MustRegister(httpcond.Type{
+		Name:        "SessionRole",
+		Description: "Match against the available session roles.",
+		Type:        conf.StringSliceType,
+		ConcatFunc:  httpcond.NewAnd,
+		Match: func(req *http.Request, value string) (bool, error) {
+			sess := FromCtx(req.Context())
+			if sess == nil {
+				return false, nil
+			}
+
+			for _, role := range sess.Roles {
+				if role == value {
+					return true, nil
+				}
+			}
+
+			return false, nil
+		},
+	})
+}
