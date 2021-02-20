903ea0fe6abde23b596661ef2e8750afdca82ab3
Patrick Pacher <patrick.pacher@gmail.com>
2021-02-20 10:28:09 +0100

    :tada: ui: add @angular/pwa, service-worker and webapp manifest

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { LayoutModule } from '@angular/cdk/layout';
 import { registerLocaleData } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import de from '@angular/common/locales/de';
@@ -5,28 +6,28 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 import { de_DE, NZ_DATE_CONFIG, NZ_I18N } from 'ng-zorro-antd/i18n';
 import { NzIconService } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { NzAvatarModule } from 'ng-zorro-antd/avatar';
-import { NzDrawerModule } from 'ng-zorro-antd/drawer';
-import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzMessageServiceModule } from 'ng-zorro-antd/message';
+import { MarkdownModule } from 'ngx-markdown';
+import { environment } from '../environments/environment';
+import { AuthorizationInterceptor } from './api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IconsProviderModule } from './icons-provider.module';
-import { AuthorizationInterceptor } from './api';
-import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { LoginModule } from './pages/login/login.module';
-import { RouterModule } from '@angular/router';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MarkdownModule } from 'ngx-markdown';
-import { DurationPipe } from './shared/pipes';
 import { SharedModule } from './shared/shared.module';
-import { NzMessageServiceModule } from 'ng-zorro-antd/message';
 
 
 registerLocaleData(de);
@@ -55,6 +56,7 @@ registerLocaleData(de);
     LoginModule,
     RouterModule,
     LayoutModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
   providers: [
     { provide: NZ_I18N, useValue: de_DE },
