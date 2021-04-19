264fc7061f633e54fbeb1d7828325c0c565dd4e3
Patrick Pacher <patrick.pacher@gmail.com>
2021-04-19 10:58:21 +0200

    :art: impr(schema): improve schema and migration handling

--- internal/schema/migration.go
+++ internal/schema/migration.go
@@ -8,10 +8,13 @@ import (
 	"sync"
 
 	"github.com/hashicorp/go-version"
+	"github.com/tierklinik-dobersberg/cis/internal/pkglog"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type MigrateFunc func(ctx context.Context, from, to *version.Version, cli *mongo.Client) error
+var log = pkglog.New("schema")
+
+type MigrateFunc func(ctx context.Context, from, to *version.Version, cli *mongo.Database) error
 
 type Migration struct {
 	// Description provides a short description of the migration.
@@ -36,30 +39,38 @@ func NewSchemaRegistry() *SchemaRegistry {
 	}
 }
 
-func (sr *SchemaRegistry) Add(migration Migration) {
+func (sr *SchemaRegistry) Add(migration ...Migration) {
 	sr.lock.Lock()
 	defer sr.lock.Unlock()
 
-	sr.migrates[migration.Key] = append(sr.migrates[migration.Key], migration)
+	for _, m := range migration {
+		sr.migrates[m.Key] = append(sr.migrates[m.Key], m)
+	}
 }
 
-func (sr *SchemaRegistry) ApplyMigrations(ctx context.Context, db Database, cli *mongo.Client) (bool, error) {
+func (sr *SchemaRegistry) ApplyMigrations(ctx context.Context, db Database, cli *mongo.Database) (bool, error) {
 	sr.lock.Lock()
 	defer sr.lock.Unlock()
 
 	migrated := false
 	for key, migrations := range sr.migrates {
-		var err error
-		migrated, err = sr.migrate(ctx, db, cli, key, migrations)
+		from, to, steps, err := sr.migrate(ctx, db, cli, key, migrations)
 		if err != nil {
 			return migrated, err
+		}
+		if from != to {
+			migrated = true
+			log.From(ctx).Infof("migrated %s from %s to %s in %s steps", key, from, to, steps)
 		}
 	}
 
 	return migrated, nil
 }
 
-func (sr *SchemaRegistry) migrate(ctx context.Context, db Database, cli *mongo.Client, key string, migrations []Migration) (bool, error) {
+func (sr *SchemaRegistry) migrate(ctx context.Context, db Database, cli *mongo.Database, key string, migrations []Migration) (
+	from,
+	to *version.Version,
+	steps int, err error) {
 	// split them up by version
 	lm := make(map[string][]Migration)
 	versions := version.Collection{}
@@ -67,7 +78,7 @@ func (sr *SchemaRegistry) migrate(ctx context.Context, db Database, cli *mongo.C
 	for _, m := range migrations {
 		ver, err := version.NewSemver(m.Version)
 		if err != nil {
-			return false, err
+			return nil, nil, 0, err
 		}
 		lm[ver.String()] = append(lm[ver.String()], m)
 		versions = append(versions, ver)
@@ -77,18 +88,18 @@ func (sr *SchemaRegistry) migrate(ctx context.Context, db Database, cli *mongo.C
 
 	current, err := db.Load(ctx, key)
 	if err != nil && !errors.Is(err, ErrNotFound) {
-		return false, err
+		return nil, nil, 0, err
 	}
 
 	var currentVersion *version.Version
 	if current != nil {
 		currentVersion, err = version.NewSemver(current.Version)
 		if err != nil {
-			return false, err
+			return nil, nil, 0, err
 		}
 	}
+	from = currentVersion
 
-	migrated := false
 	for _, ver := range versions {
 		// skip this migration if the current version is already higher.
 		if currentVersion != nil && currentVersion.GreaterThanOrEqual(ver) {
@@ -97,17 +108,17 @@ func (sr *SchemaRegistry) migrate(ctx context.Context, db Database, cli *mongo.C
 		migrations := lm[ver.String()]
 		for _, m := range migrations {
 			if err := m.MigrateFunc(ctx, currentVersion, ver, cli); err != nil {
-				return migrated, fmt.Errorf("%s -> %s: %w", currentVersion, ver, err)
+				return from, currentVersion, steps, fmt.Errorf("%s -> %s: %w", currentVersion, ver, err)
 			}
 		}
-		migrated = true
+		steps++
 		if err := db.Save(ctx, key, ver.String()); err != nil {
-			return migrated, fmt.Errorf("failed to save successfully applied migration to version %s", ver)
+			return from, currentVersion, steps, fmt.Errorf("failed to save successfully applied migration to version %s", ver)
 		}
 		currentVersion = ver
 	}
 
-	return migrated, nil
+	return from, currentVersion, steps, nil
 }
 
 // Default is the default schema registry and used by package level
@@ -115,12 +126,12 @@ func (sr *SchemaRegistry) migrate(ctx context.Context, db Database, cli *mongo.C
 var Default = NewSchemaRegistry()
 
 // Add adds m to the default schema registry.
-func Add(m Migration) {
-	Default.Add(m)
+func Add(m ...Migration) {
+	Default.Add(m...)
 }
 
 // ApplyMigrations applies all migrations added to the default schema
 // registy. See SchemaRegistry.ApplyMigrations for more information.
-func ApplyMigrations(ctx context.Context, db Database, cli *mongo.Client) (bool, error) {
+func ApplyMigrations(ctx context.Context, db Database, cli *mongo.Database) (bool, error) {
 	return Default.ApplyMigrations(ctx, db, cli)
 }
