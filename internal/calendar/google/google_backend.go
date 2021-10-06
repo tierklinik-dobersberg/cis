23c3f50ffb9162748ccd6597e358e2e2295dd453
Patrick Pacher <patrick.pacher@gmail.com>
2021-10-06 21:48:40 +0200

    :rotating_light: Add nolint for forbidigo to some files

--- internal/calendar/google/google_backend.go
+++ internal/calendar/google/google_backend.go
@@ -367,8 +367,8 @@ func credsFromFile(path string) (*oauth2.Config, error) {
 
 func getTokenFromWeb(config *oauth2.Config) (*oauth2.Token, error) {
 	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL) //nolint:forbidigo
+	fmt.Printf("Go to the following link in your browser then type the "+ //nolint:forbidigo
+		"authorization code: \n%v\n", authURL)
 
 	var authCode string
 	if _, err := fmt.Scan(&authCode); err != nil {
