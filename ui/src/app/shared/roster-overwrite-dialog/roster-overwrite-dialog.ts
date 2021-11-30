7ab5e3610808ee03d87f08039aeddbb031256c91
Patrick Pacher <patrick.pacher@gmail.com>
2021-11-30 16:39:30 +0100

    :construction: impr(ui/roster): allow to create overwrites with new dialog and limit datepicker

--- ui/src/app/shared/roster-overwrite-dialog/roster-overwrite-dialog.ts
+++ ui/src/app/shared/roster-overwrite-dialog/roster-overwrite-dialog.ts
@@ -1,13 +1,17 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, TrackByFunction } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, Optional, TrackByFunction } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzModalRef } from 'ng-zorro-antd/modal';
 import { combineLatest, forkJoin, interval, Observable, of, OperatorFunction, Subject } from 'rxjs';
-import { catchError, debounceTime, distinctUntilChanged, map, startWith, switchMap, takeUntil } from 'rxjs/operators';
-import { ConfigAPI, Day, DoctorOnDutyResponse, ExternalAPI, Overwrite, ProfileWithAvatar, QuickRosterOverwrite, RosterAPI, RosterUIConfig, UserService } from 'src/app/api';
+import { catchError, debounceTime, distinctUntilChanged, filter, map, mergeMap, startWith, switchMap, takeUntil } from 'rxjs/operators';
+import { ConfigAPI, Day, DoctorOnDutyResponse, ExternalAPI, Overwrite, OverwriteBody, ProfileWithAvatar, QuickRosterOverwrite, RosterAPI, RosterUIConfig, UserService } from 'src/app/api';
+import { extractErrorMessage } from 'src/app/utils';
 
 /**
  * FIXME:
  *  - do not allow datepicker to select in the past
- *  - change green when section is selected
+ *  - use correct date for "today" as it might depend on the current datetime (previous night shift).
+ *  - strange behavior when no roster is defined
  */
 
 interface _DoctorOnDuty extends DoctorOnDutyResponse<Date> {
@@ -24,6 +28,10 @@ interface RosterState {
     plannedNight: ProfileWithAvatar[];
 }
 
+interface _Overwrite extends Overwrite {
+  user?: ProfileWithAvatar;
+}
+
 @Component({
     templateUrl: './roster-overwrite-dialog.html',
     styleUrls: ['./roster-overwrite-dialog.scss'],
@@ -34,7 +42,7 @@ export class RosterOverwriteDialogComponent implements OnInit, OnDestroy {
     private checkOverlapping$ = new Subject<{to: Date, from: Date}>();
 
     /** A list of overwrites that overlap with the current one */
-    overlapping: Overwrite[] = [];
+    overlapping: _Overwrite[] = [];
 
     /** The currently active overwrite, if any */
     today: RosterState | null = null;
@@ -75,6 +83,73 @@ export class RosterOverwriteDialogComponent implements OnInit, OnDestroy {
     /** The custom phone number entered by the user if allowPhone is true */
     customPhoneNumber = '';
 
+    /** The actual time boundary based on the overwriteType */
+    actualBoundary: {from: Date, to: Date} | null = null;
+
+    /** Evaluates to true if everything is ready to create a new roster overwrite */
+    get valid(): boolean {
+      const valid = this.overwriteType != '' && (!!this.selectedUser || !!this.selectedQuickTargetNumber || !!this.customPhoneNumber) && !!this.from && !!this.to;
+      if (!valid) {
+        return false;
+      }
+      if (this.overwriteType === 'custom') {
+        return this.to.getTime() > this.from.getTime();
+      }
+      return true;
+    }
+
+    /** @private template-only - Returns true if d is before the current roster date */
+    isBeforeRosterDate = (d: Date): boolean => {
+      if (!d) {
+        return false;
+      }
+      const now = new Date();
+      let rosterDate = new Date(now.getFullYear(), now.getMonth(), now.getDate())
+      if (!!this.today?.onDuty && !!this.today.onDuty.rosterDate) {
+        rosterDate = new Date(this.today.onDuty.rosterDate)
+        rosterDate = new Date(rosterDate.getFullYear(), rosterDate.getMonth(), rosterDate.getDate())
+      }
+
+      return d.getTime() < rosterDate.getTime()
+    }
+
+    /** @private template-only - decided whether or not startValue should be displayed as disabled. */
+    disabledStartDate = (startValue: Date): boolean => {
+      if (!startValue) {
+        return false;
+      }
+
+      if (this.isBeforeRosterDate(startValue)) {
+        return true;
+      }
+
+      const {to} = this.actualBoundary;
+      if (!to) {
+        return false;
+      }
+      return startValue.getTime() > to.getTime()
+    }
+
+    /** @private template-only - decided whether or not endValue should be displayed as disabled. */
+    disabledEndDate = (endValue: Date): boolean => {
+      if (!endValue) {
+        return false;
+      }
+
+      const {from} = this.actualBoundary;
+      if (this.isBeforeRosterDate(endValue)) {
+        return true;
+      }
+
+      if (endValue.getTime() < (new Date()).getTime()) {
+        return true;
+      }
+      if (!from) {
+        return false;
+      }
+      return endValue.getTime() <= from.getTime();
+    }
+
     /** TrackBy function for user profiles */
     readonly trackProfile: TrackByFunction<ProfileWithAvatar> = (_: number, p: ProfileWithAvatar) => p.name;
 
@@ -87,6 +162,8 @@ export class RosterOverwriteDialogComponent implements OnInit, OnDestroy {
         private userService: UserService,
         private config: ConfigAPI,
         private cdr: ChangeDetectorRef,
+        private nzMessage: NzMessageService,
+        @Optional() public dialogRef: NzModalRef,
     ){}
 
     onDateChange() {
@@ -117,6 +194,65 @@ export class RosterOverwriteDialogComponent implements OnInit, OnDestroy {
     onCustomPhoneNumberChange() {
         this.selectedUser = '';
         this.selectedQuickTargetNumber = '';
+    }
+
+    /** @private template-only - creates a new roster overwrite */
+    createOverwrite() {
+      if (!this.valid) {
+        return;
+      }
+
+      const {from, to} = this.getTimeBoundary(this.stateForDate);
+      let body: OverwriteBody = {
+        from: from.toISOString(),
+        to: to.toISOString(),
+      }
+
+      if (!!this.selectedQuickTargetNumber) {
+        const s = this.quickSettings.find(q => q.TargetNumber === this.selectedQuickTargetNumber);
+        if (!s) {
+          this.selectedQuickTargetNumber = '';
+          // TODO(ppacher): display error message
+          return;
+        }
+        body.displayName = s.DisplayName;
+        body.phoneNumber = s.TargetNumber;
+      }
+      if (!!this.customPhoneNumber) {
+        body.phoneNumber = this.customPhoneNumber;
+        body.displayName = this.customPhoneNumber;
+      }
+
+      let target = body.displayName || '';
+      if (!!this.selectedUser) {
+        body.username = this.selectedUser;
+        target = this.userService.byName(this.selectedUser)?.fullname;
+      }
+
+      this.roster.setOverwrite(body)
+        .subscribe(
+          () => {
+          this.dialogRef?.close();
+          this.nzMessage.success(`Telefon erfolgreich auf ${target} umgeleitet.`)
+          },
+          err => this.nzMessage.error(extractErrorMessage(err, 'Telefon konnte nicht umgeleitet werden'))
+        )
+    }
+
+    /** @private template-only - deletes a overwrite by ID */
+    deleteOverwrite(ov: Overwrite) {
+      if (!ov || !ov._id) {
+        return;
+      }
+
+      this.roster.deleteOverwriteById(ov)
+        .subscribe(
+          () => {
+            this.nzMessage.success("Eintrag wurde erfolgreich gelöscht")
+            this.onDateChange();
+          },
+          err => this.nzMessage.error(extractErrorMessage(err, 'Eintrag konnte nicht gelöscht werden'))
+        )
     }
 
     ngOnInit() {
@@ -160,7 +296,7 @@ export class RosterOverwriteDialogComponent implements OnInit, OnDestroy {
 
                     // if the overwriteType is not yet set and we don't have an active overwrite we try to
                     // figure out a reasonable default
-                    if (this.overwriteType === '' && active.actual === null) {
+                    if (this.overwriteType === '') {
                         this.from = new Date();
 
                         (() => {
@@ -195,6 +331,8 @@ export class RosterOverwriteDialogComponent implements OnInit, OnDestroy {
                                 this.overwriteType = 'both';
                             }
                         })()
+
+                        this.onDateChange();
                     }
 
                     this.cdr.markForCheck();
@@ -203,36 +341,46 @@ export class RosterOverwriteDialogComponent implements OnInit, OnDestroy {
 
 
         // check for overlapping overwrites.
-        const overlapping$ =
         this.checkOverlapping$
             .pipe(
                 takeUntil(this.destroy$),
                 debounceTime(100),
-            );
-
-        overlapping$
-            .pipe(
-                switchMap(r => this.roster.getOverwrites(r.from, r.to)),
-                catchError(err => of([])),
-            )
-            .subscribe(overlapping => {
-                this.overlapping = overlapping;
-                this.cdr.markForCheck();
-            });
-
-        overlapping$
-            .pipe(
-                map(value => {
-                    if (!!value.to && !!value.from && value.from.toDateString() === value.to.toDateString()) {
-                        return value.from;
-                    }
-                    return null
+                mergeMap(value => {
+                  let start: Date | null = null;
+                  if (!!value.to && !!value.from && value.from.toDateString() === value.to.toDateString()) {
+                      start = value.from;
+                  }
+                  return forkJoin({
+                    value: of(value),
+                    state: of(start).pipe(this.toRosterState()),
+                  })
                 }),
-                this.toRosterState()
+                mergeMap(({value, state}) => {
+                  const timeBoundary = this.getTimeBoundary(state)
+                  return forkJoin({
+                    state: of(state),
+                    overlapping: this.roster.getOverwrites(timeBoundary.from, timeBoundary.to)
+                      .pipe(
+                        map(res => {
+                          return res.map(ov => ({
+                            ...ov,
+                            user: !!ov.username ? this.userService.byName(ov.username) : null,
+                          }))
+                        }),
+                        catchError(err => {
+                          if (!(err instanceof HttpErrorResponse) || err.status !== 404) {
+                            console.error(err);
+                          }
+                          return of([]);
+                        }),
+                      )
+                  })
+                })
             )
             .subscribe(state => {
-                console.log(`state for `, state);
-                this.stateForDate = state;
+                this.stateForDate = state.state;
+                this.overlapping = state.overlapping;
+                this.actualBoundary = this.getTimeBoundary(state.state);
                 this.cdr.markForCheck();
             })
     }
@@ -263,6 +411,43 @@ export class RosterOverwriteDialogComponent implements OnInit, OnDestroy {
         this.preferredUsers.forEach(matchUser);
         this.otherAllowedUsers.forEach(matchUser);
         return result;
+    }
+
+    private getTimeBoundary(state: RosterState = this.stateForDate): {from: Date, to: Date} {
+      if (this.overwriteType === 'custom') {
+        return {
+          from: this.from || null,
+          to: this.to || null,
+        }
+      }
+      if (!this.from || !state || !state.planned) {
+          return null;
+      }
+      let from: Date;
+      let to: Date;
+
+      switch (this.overwriteType) {
+        case 'both':
+          from = new Date(state.planned.onCall.dayStart!);
+          to = new Date(state.nextDay.onCall.dayStart!);
+          break;
+
+        case 'day-shift':
+          from = new Date(state.planned.onCall.dayStart!);
+          to = new Date(state.planned.onCall.nightStart!);
+          break;
+
+        case 'night-shift':
+          from = new Date(state.planned.onCall.nightStart!);
+          to = new Date(state.nextDay.onCall.dayStart!);
+          break;
+
+        default:
+          console.error(`Invalid overwriteType ${this.overwriteType}`)
+          return null;
+      }
+
+      return {from, to}
     }
 
     private toRosterState(): OperatorFunction<Date, RosterState> {
@@ -304,17 +489,19 @@ export class RosterOverwriteDialogComponent implements OnInit, OnDestroy {
                                     catchError(err => {
                                         if (!(err instanceof HttpErrorResponse) || err.status !== 404 ) {
                                             console.error(`Failed to get active roster`, err)
+                                            return of(null);
                                         }
-                                        return of(null as (Day|null));
+                                        return of(err.error);
                                     }),
                                 ),
                             nextDay: this.roster.forDay(new Date(d.getFullYear(), d.getMonth(), d.getDate()+1))
                                 .pipe(
                                     catchError(err => {
                                         if (!(err instanceof HttpErrorResponse) || err.status !== 404 ) {
-                                            console.error(`Failed to get active roster`, err)
+                                          console.error(`Failed to get active roster`, err)
+                                          return of(null as (Day|null));
                                         }
-                                        return of(null as (Day|null));
+                                        return of(err.error)
                                     }),
                                 ),
                         })
@@ -326,8 +513,8 @@ export class RosterOverwriteDialogComponent implements OnInit, OnDestroy {
                     }
                     return {
                         ...result,
-                        plannedDay: result?.planned?.onCall.day.map(u => this.userService.byName(u)),
-                        plannedNight: result?.planned?.onCall.night.map(u => this.userService.byName(u)),
+                        plannedDay: result?.planned?.onCall.day?.map(u => this.userService.byName(u)),
+                        plannedNight: result?.planned?.onCall.night?.map(u => this.userService.byName(u)),
                     }
                 })
             )
@@ -347,7 +534,12 @@ export class RosterOverwriteDialogComponent implements OnInit, OnDestroy {
 
         cfg.EligibleRolesForOverwrite.forEach(
             role => this.userService.byRole(role).forEach(
-                user => preferred.add(user)
+                user => {
+                  if (user.disabled) {
+                    return;
+                  }
+                  preferred.add(user)
+                }
             )
         );
 
@@ -355,6 +547,9 @@ export class RosterOverwriteDialogComponent implements OnInit, OnDestroy {
         // as well
         if (cfg.AllowAnyUserAsOverwrite) {
             this.userService.snapshot.forEach(user => {
+                if (user.disabled) {
+                  return
+                }
                 if (!preferred.has(user)) {
                     otherUsers.add(user);
                 }
