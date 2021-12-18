be9a079ea4249af22949d20f3f74f136dffbb645
Patrick Pacher <patrick.pacher@gmail.com>
2021-12-18 09:25:02 +0100

    :sparkles: impr(ui): update calllog page to new styling and use new search api

--- ui/src/app/pages/calllogs/calllog.ts
+++ ui/src/app/pages/calllogs/calllog.ts
@@ -11,11 +11,12 @@ import { HeaderTitleService } from 'src/app/shared/header-title';
 })
 export class CallLogComponent implements OnInit, OnDestroy {
   private subscriptions = Subscription.EMPTY;
-  private selectedDate = new BehaviorSubject<Date>(new Date());
+  private selectedDate = new BehaviorSubject<Date[]>([]);
 
-  date: Date;
+  dateRange: Date[] = [];
 
   distinctCallers: Set<string> = new Set();
+  missedCalls = 0;
   totalCallTime = 0;
   logs: CallLogModel[] = [];
   loading = false;
@@ -27,14 +28,20 @@ export class CallLogComponent implements OnInit, OnDestroy {
     public layout: LayoutService,
   ) { }
 
-  onChange(date: Date): void {
+  onChange(date: Date[]): void {
     this.selectedDate.next(date);
   }
 
   ngOnInit(): void {
-    this.header.set('Anruf Journal');
-    this.date = new Date();
+    this.header.set('Anruf Journal', 'Protokol der eingehenden und ausgehenden Telefonate.');
     this.subscriptions = new Subscription();
+
+    const now = new Date();
+    this.dateRange = [
+      new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0),
+      new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59)
+    ]
+    this.selectedDate.next(this.dateRange);
 
     const sub =
       combineLatest([
@@ -46,20 +53,29 @@ export class CallLogComponent implements OnInit, OnDestroy {
           mergeMap(() => this.selectedDate),
           mergeMap(d => {
             this.loading = true;
-            const year = d.getFullYear();
-            const month = d.getMonth() + 1;
-            const day = d.getDate();
 
-            return this.calllogapi.forDate(year, month, day);
+            if (!!d[0] && !!d[1]) {
+              return this.calllogapi.search({
+                from: d[0],
+                to: d[1],
+              })
+            }
+            return this.calllogapi.search({at: d[0]});
           }),
         )
         .subscribe(logs => {
-          this.logs = logs;
-
+          this.missedCalls = 0;
+          this.logs = logs.items;
           this.distinctCallers = new Set();
           this.logs.forEach(log => {
             this.distinctCallers.add(log.caller);
+            if (!log.callType) {
+              this.missedCalls++;
+            }
           });
+
+
+
           setTimeout(() => {
             this.loading = false;
           }, 300);
