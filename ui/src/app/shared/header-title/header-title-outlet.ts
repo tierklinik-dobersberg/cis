4f93d2c80a270e34356cfe9ef21f8df9a1889534
Patrick Pacher <patrick.pacher@gmail.com>
2021-12-27 15:59:54 +0100

    :iphone: impr(ui): fix word-wrapping in page header

--- ui/src/app/shared/header-title/header-title-outlet.ts
+++ ui/src/app/shared/header-title/header-title-outlet.ts
@@ -10,7 +10,7 @@ import { HeaderTitleService, PageHeader } from './header.service';
     </div>
     <div class="flex flex-col">
       <span class="text-base font-semibold text-color-primary-dark font-lato">{{ header.title }}</span>
-      <span *ngIf="header.description" class="hidden text-sm text-secondary font-iter sm:block">{{ header.description }}</span>
+      <span *ngIf="header.description" class="hidden text-sm break-normal text-secondary font-iter sm:block">{{ header.description }}</span>
     </div>
   `,
   styles: [
@@ -19,7 +19,6 @@ import { HeaderTitleService, PageHeader } from './header.service';
             display: inline-flex;
             flex-direction: row;
             align-self: center;
-            white-space: nowrap;
         }
         `
   ],
