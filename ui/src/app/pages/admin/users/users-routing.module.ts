6e557001ef80c32de61ab36664d47b393b5ff3e5
Patrick Pacher <patrick.pacher@gmail.com>
2022-02-24 20:10:35 +0100

    :recycle: refactor(ui): rename component create-user to manage-user

--- ui/src/app/pages/admin/users/users-routing.module.ts
+++ ui/src/app/pages/admin/users/users-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
-import { CreateUserComponent } from './create-user';
+import { ManageUserComponent } from './manage-user';
 import { RoleListComponent } from './role-list';
 import { RoleViewComponent } from './role-view';
 import { UserListComponent } from './user-list';
 
 let routes: Route[] = [
   {path: 'users', component: UserListComponent},
-  {path: 'users/create', component: CreateUserComponent},
-  {path: 'users/edit/:username', component: CreateUserComponent},
+  {path: 'users/create', component: ManageUserComponent},
+  {path: 'users/edit/:username', component: ManageUserComponent},
 
   {path: 'roles', component: RoleListComponent},
   {path: 'roles/create', component: RoleViewComponent},
