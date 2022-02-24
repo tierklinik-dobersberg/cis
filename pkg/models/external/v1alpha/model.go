952d48bd82c0e13aa45ea7b9bacb11bfa055b35b
Patrick Pacher <patrick.pacher@gmail.com>
2022-02-24 20:02:24 +0100

    :recycle: wip(api/external): add source of day/night shift start times to API response

--- pkg/models/external/v1alpha/model.go
+++ pkg/models/external/v1alpha/model.go
@@ -10,10 +10,11 @@ type DoctorOnDuty struct {
 }
 
 type DoctorOnDutyResponse struct {
-	Doctors      []DoctorOnDuty `json:"doctors"`
-	Until        time.Time      `json:"until"`
-	RosterDate   string         `json:"rosterDate"` // YYYY-MM-DD in the local timezone
-	IsOverwrite  bool           `json:"isOverwrite"`
-	IsDayShift   bool           `json:"isDayShift"`
-	IsNightShift bool           `json:"isNightShift"`
+	Doctors       []DoctorOnDuty `json:"doctors"`
+	Until         time.Time      `json:"until"`
+	RosterDate    string         `json:"rosterDate"` // YYYY-MM-DD in the local timezone
+	IsOverwrite   bool           `json:"isOverwrite"`
+	IsDayShift    bool           `json:"isDayShift"`
+	IsNightShift  bool           `json:"isNightShift"`
+	EndTimeSource string         `json:"endTimeSource"`
 }
