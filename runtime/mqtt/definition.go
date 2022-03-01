ac83af0f315805f15f2b5691fd77ad45b293fe35
Patrick Pacher <patrick.pacher@gmail.com>
2022-03-01 22:06:33 +0100

    :truck: move: move mailer, mqtt and twilio to "integration" category

--- runtime/mqtt/definition.go
+++ runtime/mqtt/definition.go
@@ -131,6 +131,7 @@ func addMqttSchema(cs *runtime.ConfigSchema) error {
 		SVGData:     `<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />`,
 		Spec:        ConnectionConfigSpec,
 		Multi:       true,
+		Category:    "Integration",
 		Annotations: new(conf.Annotation).With(
 			runtime.Unique("Name"),
 			runtime.OverviewFields("Name", "Server", "User", "ClientID"),
