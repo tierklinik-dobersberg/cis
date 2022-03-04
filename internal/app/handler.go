1267e9e536b2bafd719b616bc3d34e41059f5646
Patrick Pacher <patrick.pacher@gmail.com>
2022-03-04 15:14:56 +0100

    :children_crossing: impr(app): add otel tracing span in WrapHandler

--- internal/app/handler.go
+++ internal/app/handler.go
@@ -2,8 +2,11 @@ package app
 
 import (
 	"context"
+	"reflect"
+	"runtime"
 
 	"github.com/labstack/echo/v4"
+	"go.opentelemetry.io/otel"
 )
 
 // HandlerFunc defines a cis HTTP handler func.
@@ -11,7 +14,17 @@ type HandlerFunc func(ctx context.Context, app *App, c echo.Context) error
 
 // WrapHandler wraps a handler function into a gin.HandlerFunc.
 func WrapHandler(app *App, fn HandlerFunc) echo.HandlerFunc {
+	fName := getFunctionName(fn)
+
 	return func(c echo.Context) error {
-		return fn(c.Request().Context(), app, c)
+		ctx := c.Request().Context()
+		ctx, sp := otel.Tracer("").Start(ctx, fName)
+		defer sp.End()
+
+		return fn(ctx, app, c)
 	}
+}
+
+func getFunctionName(i interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
 }
