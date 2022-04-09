0ecc26a2e65585ff95ecb6ae7ff3ed97c6db4014
Patrick Pacher <patrick.pacher@gmail.com>
2022-04-09 09:53:12 +0200

    :bug: fix(ui): display username in welcome page if no fullname is set

--- ui/src/app/pages/welcome/welcome.component.ts
+++ ui/src/app/pages/welcome/welcome.component.ts
@@ -1,14 +1,9 @@
-import { Component, isDevMode, OnDestroy, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { BehaviorSubject, combineLatest, interval, Subscription } from 'rxjs';
-import { delay, mergeMap, repeatWhen, retryWhen, startWith } from 'rxjs/operators';
-import { DoorAPI, IdentityAPI, Permissions, Roster, RosterAPI, State, VoiceMailAPI } from 'src/app/api';
+import { Component, isDevMode, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { delay, retryWhen } from 'rxjs/operators';
+import { IdentityAPI, Permissions, VoiceMailAPI } from 'src/app/api';
 import { LayoutService } from 'src/app/services';
 import { HeaderTitleService } from 'src/app/shared/header-title';
-
-interface Dummy {
-  name: string;
-  busyTimes: [number, number][];
-}
 
 @Component({
   selector: 'app-welcome',
@@ -42,7 +37,6 @@ export class WelcomeComponent implements OnInit, OnDestroy {
     private header: HeaderTitleService,
     private identityapi: IdentityAPI,
     private voicemailapi: VoiceMailAPI,
-    private rosterapi: RosterAPI,
     public layout: LayoutService,
   ) { }
 
@@ -50,31 +44,10 @@ export class WelcomeComponent implements OnInit, OnDestroy {
 
   private allSub = new Subscription();
 
-  /** TEST CODE */
-  currentStep = 0;
-  slots = Array.from(new Array(4 * 10).keys());
-  offStart = 4 * 4;
-  offEnd = 4 * 6;
-  startIdx: number | null = null;
-  busyUser: Dummy | null = null;
-  users: Dummy[] = [
-    {
-      name: 'Mag. Carmen Pacher',
-      busyTimes: [
-        [0, 4],
-        [22, 28],
-      ]
-    },
-    {
-      name: 'Dr. Claudia Fürst',
-      busyTimes: [
-        [0, 3],
-      ]
-    }
-  ];
-
   ngOnInit(): void {
-    this.header.set(`Hallo ${this.identityapi.currentProfile?.fullname},`, 'Hier findest du eine Übersicht der wichtigsten Informationen.');
+    const name = this.identityapi.currentProfile?.fullname || this.identityapi.currentProfile?.name;
+
+    this.header.set(`Hallo ${name},`, 'Hier findest du eine Übersicht der wichtigsten Informationen.');
 
     this.voicemailapi.listMailboxes()
       .pipe(retryWhen(e => e.pipe(delay(10000))))
@@ -87,43 +60,4 @@ export class WelcomeComponent implements OnInit, OnDestroy {
     this.allSub.unsubscribe();
   }
 
-  isBusy(idx: number, user: Dummy): boolean {
-    return user.busyTimes.some(time => idx >= time[0] && idx <= time[1]);
-  }
-
-  startBusy(startIdx: number, user: Dummy, event: MouseEvent): void {
-    this.startIdx = startIdx;
-    this.busyUser = user;
-  }
-
-  getTime(slot: number): string {
-    const hour = 8 + Math.floor(slot / 4);
-    const min = (slot % 4) * 15;
-    const pad = (val: number) => {
-      const s = `${val}`;
-      if (s.length < 2) {
-        return `0${s}`;
-      }
-      return s;
-    };
-
-    return pad(hour) + ':' + pad(min);
-  }
-
-  endBusy(endIdx: number, user: Dummy, event: MouseEvent): void {
-    if (this.startIdx === null) {
-      return;
-    }
-
-    if (user !== this.busyUser) {
-      this.busyUser = null;
-      this.startIdx = null;
-      return;
-    }
-
-    user.busyTimes.push([this.startIdx, endIdx]);
-    this.startIdx = null;
-    this.busyUser = null;
-    this.currentStep++;
-  }
 }
